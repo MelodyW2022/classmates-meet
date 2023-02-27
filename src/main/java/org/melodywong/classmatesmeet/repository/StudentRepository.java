7683d40eef06b833d16629b55b1851400fe60bc4
Sheung Wan Wong <melodywonghxy@hotmail.com>
2023-02-26 20:14:46 -0700

    created StudentRepository

---
+++ src/main/java/org/melodywong/classmatesmeet/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package org.melodywong.classmatesmeet.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.melodywong.classmatesmeet.model.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+
+}
