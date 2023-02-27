7ea245d4b3c0c6a6744710989e5fedf291b04ede
Sheung Wan Wong <melodywonghxy@hotmail.com>
2023-02-26 19:27:46 -0700

    set up a spring boot project and configure MySQL

---
+++ src/main/java/org/melodywong/classmatesmeet/ClassmatesmeetApplication.java
@@ -0,0 +1,13 @@
+package org.melodywong.classmatesmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmatesmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassmatesmeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/melodywong/classmatesmeet/ClassmatesmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.melodywong.classmatesmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassmatesmeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
